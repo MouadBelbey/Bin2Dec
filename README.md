c9e4163a5ef15f599df2aedbcfaf14545c2897b7
MouadB <mouadbelbey@yahoo.fr>
2022-10-03 11:56:31 -0400

    Commiting the final result

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bin2Dec
 
-Starting the app
+- This Web application converts a binary number given by the user to a decimal number.
+- The user is required to enter a number.
+- The user is limited to 8 numbers.
--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 let ul = document.querySelector(".list-group")
 
-
-
 // grapping the number
 
 const numForm = document.forms['submit_form'];
 
 numForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    const value = numForm.querySelector('input[type="number"]').value;
+    const value = numForm.querySelector('input[type="text"]').value;
     let result = parseInt(value, 2);
 
-    // create elements
-    let li = document.createElement('li');
-    let resultNumber = document.createElement('span');
-
-    // adding style to the list
-    li.className = 'list-group-item';
-
+    const input = document.querySelector('.binary');
+    
     // add content
-    resultNumber.textContent = result;
+    document.getElementById('decimal').innerHTML = input.value + " => " + result;
 
-    // append to document
-    li.appendChild(resultNumber);
-    ul.appendChild(li);
-
+    // clear input field
+    input.value = '';
+    
 
 })
